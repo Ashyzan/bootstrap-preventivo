3c038905091555606091498366192f40487971d5
Ashyzan <info@studiocreativo69.it>
2024-04-03 01:04:34 +0200

    ADD Javascript conditions

--- js/app.js
+++ js/app.js
@@ -8,75 +8,84 @@
 // se la commissione riguarda l’analisi progettuale il prezzo orario è di 33.60€/l’ora
 
 // prezzo orario sviluppo backend
-const prezzoOrarioUno = 20.50 //tipo number
+//const prezzoOrarioUno = 20.50 //tipo number
 
 // prezzo orario sviluppo frontend
-const prezzoOrarioDue = 15.30 //tipo number
+//const prezzoOrarioDue = 15.30 //tipo number
 
 // prezzo orario analisi progettuale
-const prezzoOrarioTre = 33.60 //tipo number
+//const prezzoOrarioTre = 33.60 //tipo number
 
-let prezzoSelezionato = document.getElementById("job").value; // tipo stringa
+let prezzoSelezionato = parseFloat(document.getElementById("job").value); // tipo number
+//console.log(prezzoSelezionato)
 
-console.log(prezzoSelezionato)
+let prezzoJob = 0
+//let jobRequest = false
 
-let prezzoFinale = '0'
-
-if (prezzoSelezionato === '1') {
-    let prezzoFinale = prezzoOrarioUno * 10
-
-    
+if (prezzoSelezionato === 1) {
+    // jobRequested = true
+    prezzoJob = 20.50 * 10
 
 }
-console.log(prezzoFinale, 'il prezzo finale è 205')
-//  else if (prezzoSelezionato === 2) {
-//     let prezzoFinale = prezzoOrarioDue * 10
-    
-//     console.log(prezzoFinale, 'il prezzo finale è 153')
-// }
+//console.log(jobRequest, prezzofinale, 'il prezzo finale è 205')
+else if (prezzoSelezionato === 2) {
+    //  jobRequested = true
+    prezzoJob = 15.30 * 10
+
+}
+//console.log(prezzofinale, 'il prezzo finale è 153')
 
 
+else if (prezzoSelezionato === 3) {
+    // jobRequested = true
+    prezzoJob = 33.60 * 10
+}
 
-//   else if (prezzoSelezionato === 3) {
-//     let prezzoFinale = prezzoOrarioTre * 10
-//     console.log(prezzoFinale, 'il prezzo finale è 336')
-// }
 
+else {
+    // jobRequested = false
+    prezzoJob = 0
 
-// recupero il tipo di prezzo dal valore del select sul form
-    // const name = document.getElementById('name').value;
-	// const surname = document.getElementById('surname').value;
-	// const age = document.getElementById('age').value;
+}
+console.log('il prezzo selezionato è di €', prezzoJob)
+
 
 // L’utente potrebbe decidere di utilizzare un codice 
 // promozionale tra i seguenti: YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24.
 
 // creo un array di codici promozionali
 
-// const couponValido = [YHDNU32, JANJC63, PWKCN25, SJDPO96, POCIE24]
+const couponValido = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24']
 
-// let couponInserito = // mettere il valore preso dal campo del form codice promozionale
-
-// let scontoValido = couponValido.includes(couponInserito)
-
+let couponInserito = document.getElementById("promocode").value; // tipo number
+let scontoValido = couponValido.includes(couponInserito)
+let sconto = 0
 // Se l’utente inserisce un codice promozionale valido, ha diritto ad uno sconto 
 // del 25% sul prezzo finale.
+
+let prezzoFinale = prezzoJob - sconto
+
+if (scontoValido===true) {
+    sconto = prezzoJob * 0.25
+    prezzoFinale = prezzoJob - sconto
+    document.getElementById("result").innerHTML = prezzoFinale + ' €'
+
+}
+
+else {
+
+    document.getElementById("result").innerHTML = prezzoFinale + ' €' + ' sconto non applicabile'
+
+
+}
+
+
+
+
+// console.log( 'lo sconto è di', sconto)
+// console.log('il prezzo finale è di ' ,prezzoFinale)
+
 //Se il codice inserito non è valido, 
-// il sito deve informare l’utente che il codice non è valido e il prezzo finale viene calcolato senza applicare sconti.
+// il sito deve informare l’utente che il codice non è valido 
+//e il prezzo finale viene calcolato senza applicare sconti.
 
-// if ()
-
-
-// if (trovato === true) {
-
-//     console.log('codice valido')
-//     console.log(trovato)
-
-
-// }
-// else {
-//     console.log('spiacente, il codice non funziona')
-//     console.log(trovato)
-// }
-
-// const prezzoFinale = 
